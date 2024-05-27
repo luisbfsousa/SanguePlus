9c6cf91347fc6471c8cd14b8cc854a01827df659
luisbfsousa <luisbfsousa@ua.pt>
2024-05-27 02:12:30 +0100

    org

--- others-/killProcedures.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @sql NVARCHAR(MAX) = '';
-
-SELECT @sql += 'DROP PROCEDURE ' + QUOTENAME(SCHEMA_NAME(schema_id)) + '.' + QUOTENAME(name) + '; '
-FROM sys.procedures;
-
-EXEC sp_executesql @sql;
--- others-/killViews.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @sql NVARCHAR(MAX) = '';
-
-SELECT @sql += 'DROP VIEW ' + QUOTENAME(SCHEMA_NAME(schema_id)) + '.' + QUOTENAME(name) + '; '
-FROM sys.views;
-
-EXEC sp_executesql @sql;
